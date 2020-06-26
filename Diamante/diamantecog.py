e3de3c5d0d3d611ecad917cf3bfb2d6762b7e580
AndreiCNIM <37911417+AndreiCNIM@users.noreply.github.com>
2020-06-26 20:28:19 +0300

    Update diamantecog.py

--- Diamante/diamantecog.py
+++ Diamante/diamantecog.py
@@ -96,6 +96,6 @@ class Diamante(commands.Cog):
     async def removeyoutubeid(self, ctx):
         with open(str(cog_data_path(self) / "data.json"), "r+") as data_file:
             data = json.load(data_file)
-            del data[str(ctx.author.id)]
+            data.pop(str(ctx.author.id), None)
             data_file.seek(0)
             json.dump(data, data_file)
