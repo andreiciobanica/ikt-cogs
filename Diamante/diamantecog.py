c3d67c21c3985813c92ce1014f299ce0674328d3
AndreiCNIM <37911417+AndreiCNIM@users.noreply.github.com>
2020-06-26 23:49:01 +0300

    Update diamantecog.py

--- Diamante/diamantecog.py
+++ Diamante/diamantecog.py
@@ -98,6 +98,6 @@ class Diamante(commands.Cog):
         pathdb = str(cog_data_path(self) / "database" / dirs[0])
         
         ScriptDatabase = self.InstancedDatabase(pathdb)
-        ScriptDatabase.InstancedDatabase.execute('SELECT * from diamante WHERE userid = "32u"')
+        ScriptDatabase.execute.InstancedDatabase('SELECT * from diamante WHERE userid = "32u"')
             
         await ctx.send(print(ScriptDatabase.fetchone()))
