b286d11b3af9148c9be2b3653ccc01cadf1df2bf
AndreiCNIM <37911417+AndreiCNIM@users.noreply.github.com>
2020-06-26 04:13:57 +0300

    Update diamantecog.py

--- Diamante/diamantecog.py
+++ Diamante/diamantecog.py
@@ -67,7 +67,7 @@ class Diamante(commands.Cog):
         f_ = drive.CreateFile({'id': idfile})
         f_.GetContentFile(numefile)
         await ctx.send("Baza de date s-a încărcat!")
-        await ctx.send(str(client.user.id))
+        await ctx.send(str(Config.user().id))
         global ScriptDatabase
         pathdb = os.path.abspath(numefile)
         numedb = os.path.join(os.path.dirname(__file__), numefile)
