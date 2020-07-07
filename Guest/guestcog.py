5f73b5e73a5b4f1d46a3cdc7ea61cf4d77004cf2
AndreiCNIM <37911417+AndreiCNIM@users.noreply.github.com>
2020-07-08 00:37:32 +0300

    Update guestcog.py

--- Guest/guestcog.py
+++ Guest/guestcog.py
@@ -1,6 +1,9 @@
 from redbot.core import commands
 
 class Guest(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
     @commands.command(name="getguests")
     async def getGuests(self, ctx):
         await ctx.send("A")
