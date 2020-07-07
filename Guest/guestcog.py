10fde19cf47714b1ad695b2710804141b909ef45
AndreiCNIM <37911417+AndreiCNIM@users.noreply.github.com>
2020-07-08 01:15:06 +0300

    Update guestcog.py

--- Guest/guestcog.py
+++ Guest/guestcog.py
@@ -8,5 +8,5 @@ class Guest(commands.Cog):
     async def getGuests(self, ctx):
         for member in server.members:
             for role in member.roles: 
-                if role.id == "730173883151286285":
+                if role.id == 730173883151286285:
                     await ctx.send("a")
