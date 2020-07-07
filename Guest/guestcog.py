fa5e885a99ffa978c59c6f18cad1814fbc6828dc
AndreiCNIM <37911417+AndreiCNIM@users.noreply.github.com>
2020-07-08 00:35:10 +0300

    Update guestcog.py

--- Guest/guestcog.py
+++ Guest/guestcog.py
@@ -3,7 +3,4 @@ from redbot.core import commands
 class Guest(commands.Cog):
     @commands.command(name="getguests")
     async def getGuests(self, ctx):
-        for member in server.members:
-            for role in member.roles: 
-                if role.id == "730173883151286285":
-                    await ctx.send("A")
+        await ctx.send("A")
