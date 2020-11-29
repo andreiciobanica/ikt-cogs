d45b4c674b22410aefc52ec0b369966633accb6e
AndreiCNIM <37911417+AndreiCNIM@users.noreply.github.com>
2020-11-29 23:53:56 +0200

    Update notificareyt.py

--- Notificare/notificareyt.py
+++ Notificare/notificareyt.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from redbot.core import commands
 from datetime import datetime, timedelta, timezone
+from pytz import timezone
 import discord
 
 class Notificare(commands.Cog):
@@ -14,10 +15,11 @@ class Notificare(commands.Cog):
         channel = self.bot.get_channel(440957219593519126)
         logs_channel = self.bot.get_channel(715676435372834927)
         await channel.set_permissions(ctx.guild.default_role, send_messages=False)
+        tz = timezone("Europe/Bucharest")
         embed = discord.Embed(title="Blocare chat 🤫", color=0xefe125)
         embed.add_field(name="NUME", value="ID", inline=True)
         embed.add_field(name=str(ctx.author.name), value=str(ctx.author.id), inline=True)
-        embed.set_footer(text=str(datetime.now().strftime("%d %B %Y %H:%M:%S")))
+        embed.set_footer(text=str(datetime.now(tz).strftime("%d %B %Y %H:%M:%S")))
         await logs_channel.send(embed=embed)
     
     @commands.mod()
@@ -26,8 +28,9 @@ class Notificare(commands.Cog):
         channel = self.bot.get_channel(440957219593519126)
         logs_channel = self.bot.get_channel(715676435372834927)
         await channel.set_permissions(ctx.guild.default_role, send_messages=None)
+        tz = timezone("Europe/Bucharest")
         embed = discord.Embed(title="Deblocare chat 🤫", color=0xefe125)
         embed.add_field(name="NUME", value="ID", inline=True)
         embed.add_field(name=str(ctx.author.name), value=str(ctx.author.id), inline=True)
-        embed.set_footer(text=str(datetime.now().strftime("%d %B %Y %H:%M:%S")))
+        embed.set_footer(text=str(datetime.now(tz).strftime("%d %B %Y %H:%M:%S")))
         await logs_channel.send(embed=embed)
