af4ca321f6fbcf99908cc4fe51fec2303d10c9a5
AndreiCNIM <37911417+AndreiCNIM@users.noreply.github.com>
2020-11-29 23:13:47 +0200

    Update notificareyt.py

--- Notificare/notificareyt.py
+++ Notificare/notificareyt.py
@@ -7,16 +7,16 @@ class Notificare(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="blocarechat")
+    @commands.mod(name="blocarechat")
     async def blocarechat(self, ctx):
         channel = self.bot.get_channel(440957219593519126)
         logs_channel = self.bot.get_channel(715676435372834927)
         await channel.set_permissions(ctx.guild.default_role, send_messages=False)
-        await logs_channel.send("Chat-ul a fost blocat de <@" + str(ctx.author.name) + ">")
+        await logs_channel.send("Chat-ul a fost blocat de " + str(ctx.author.name) + " id: " + str(ctx.author.id))
     
-    @commands.command(name="deblocarechat")
+    @commands.mod(name="deblocarechat")
     async def deblocarechat(self, ctx):
         channel = self.bot.get_channel(440957219593519126)
         logs_channel = self.bot.get_channel(715676435372834927)
         await channel.set_permissions(ctx.guild.default_role, send_messages=None)
-        await logs_channel.send("Chat-ul a fost deblocat!" + str(ctx.author.id))
+        await logs_channel.send("Chat-ul a fost deblocat de " + str(ctx.author.name) + " id: " + str(ctx.author.id))
