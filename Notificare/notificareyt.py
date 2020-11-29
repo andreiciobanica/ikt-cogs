6a0be3700d546e0af56b9564011ad0fbf11b39f3
AndreiCNIM <37911417+AndreiCNIM@users.noreply.github.com>
2020-11-29 23:16:42 +0200

    Update notificareyt.py

--- Notificare/notificareyt.py
+++ Notificare/notificareyt.py
@@ -7,14 +7,16 @@ class Notificare(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.mod.command(name="blocarechat")
+    @commands.mod()
+    @commands.command(name="blocarechat")
     async def blocarechat(self, ctx):
         channel = self.bot.get_channel(440957219593519126)
         logs_channel = self.bot.get_channel(715676435372834927)
         await channel.set_permissions(ctx.guild.default_role, send_messages=False)
         await logs_channel.send("Chat-ul a fost blocat de " + str(ctx.author.name) + " id: " + str(ctx.author.id))
     
-    @commands.mod.command(name="deblocarechat")
+    @commands.mod()
+    @commands.command(name="deblocarechat")
     async def deblocarechat(self, ctx):
         channel = self.bot.get_channel(440957219593519126)
         logs_channel = self.bot.get_channel(715676435372834927)
