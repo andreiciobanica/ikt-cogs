895b0854bf476c856955b7457e33f05f2100a78b
AndreiCNIM <37911417+AndreiCNIM@users.noreply.github.com>
2020-11-29 23:00:00 +0200

    Update notificareyt.py

--- Notificare/notificareyt.py
+++ Notificare/notificareyt.py
@@ -10,9 +10,13 @@ class Notificare(commands.Cog):
     @commands.command(name="blocarechat")
     async def blocarechat(self, ctx):
         channel = self.bot.get_channel(440957219593519126)
+        logs_channel = self.bot.get_channel(715676435372834927)
         await channel.set_permissions(ctx.guild.default_role, send_messages=False)
+        await logs_channel.send("Chat-ul a fost blocat!" + ctx.author.id)
     
     @commands.command(name="deblocarechat")
     async def deblocarechat(self, ctx):
         channel = self.bot.get_channel(440957219593519126)
+        logs_channel = self.bot.get_channel(715676435372834927)
         await channel.set_permissions(ctx.guild.default_role, send_messages=None)
+        await logs_channel.send("Chat-ul a fost deblocat!" + ctx.author.id)
