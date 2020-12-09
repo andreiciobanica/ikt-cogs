53dd6cd375ab3142e71c47df479a9f7a44d6d0b2
AndreiCNIM <37911417+AndreiCNIM@users.noreply.github.com>
2020-12-09 20:23:58 +0200

    Update notificareyt.py

--- Notificare/notificareyt.py
+++ Notificare/notificareyt.py
@@ -46,3 +46,14 @@ class Notificare(commands.Cog):
         await channel.set_permissions(ctx.guild.default_role, send_messages=False, mention_everyone=False)
         mesajfinal = linkyoutube+"\n\n:purple_heart:  Nu uitați de like, un share este binevenit și dacă nu ați făcut-o până acum, nu uita să te abonezi și să apeși clopoțelul pentru a fi întotdeauna notificat când IKatheryne face live sau postează! :purple_heart:"
         await channel.send(ctx.message.guild.default_role.name + " " + mesajfinal, allowed_mentions=discord.AllowedMentions(roles=True, users=True, everyone=True))
+    
+    @commands.mod()
+    @commands.command(name="notificaretwitch")
+    async def notificaretwitch(self, ctx):
+        channel = self.bot.get_channel(440957219593519126)
+        await channel.set_permissions(ctx.guild.default_role, send_messages=False, mention_everyone=False)
+        mesajfinal = "https://www.twitch.tv/ikatheryne"+"\n\n:purple_heart: IKatheryne este acum live pe Twitch. Vă reamintim faptul că live-ul este în limba germană și engleză, limba română pe chat/voice nu este acceptată! :purple_heart:"
+        await channel.send(ctx.message.guild.default_role.name + " " + mesajfinal, allowed_mentions=discord.AllowedMentions(roles=True, users=True, everyone=True))
+        
+    
+    
