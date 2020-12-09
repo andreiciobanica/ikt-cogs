5131f64125b09b4ebdd1ac4f6ff0150fd4ec2257
AndreiCNIM <37911417+AndreiCNIM@users.noreply.github.com>
2020-12-09 19:42:08 +0200

    Update notificareyt.py

--- Notificare/notificareyt.py
+++ Notificare/notificareyt.py
@@ -41,7 +41,7 @@ class Notificare(commands.Cog):
     
     @commands.mod()
     @commands.command(name="notificareyoutube", pass_context = True)
-    async def notificareyoutube(self, ctx, linkyoutube):
+    async def notificareyoutube(self, ctx, linkyoutube, *args, **kwargs):
         channel = self.bot.get_channel(440957219593519126)
         mesajfinal = linkyoutube+"\n\n:purple_heart:  Nu uitați de like, un share este binevenit și dacă nu ați făcut-o până acum, nu uita să te abonezi și să apeși clopoțelul pentru a fi întotdeauna notificat când IKatheryne face live sau postează! :purple_heart:"
         await channel.send(ctx.message.guild.default_role, allowed_mentions=discord.AllowedMentions(everyone=False, users=False, roles=False))
