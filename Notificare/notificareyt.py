03fdd6983a2fbfca129c06efb5620e3e83afc71e
AndreiCNIM <37911417+AndreiCNIM@users.noreply.github.com>
2020-12-09 19:09:40 +0200

    Update notificareyt.py

--- Notificare/notificareyt.py
+++ Notificare/notificareyt.py
@@ -42,7 +42,7 @@ class Notificare(commands.Cog):
     @commands.mod()
     @commands.command(name="notificareyoutube")
     async def notificareyoutube(self, ctx, linkyoutube):
-        channel = self.bot.get_channel(448252534054715392)
+        channel = self.bot.get_channel(440957219593519126)
         await channel.set_permissions(ctx.guild.default_role, send_messages=False, mention_everyone=False)
         mesajfinal = "@everyone, "+linkyoutube+"\n\n:purple_heart:  Nu uitați de like, un share este binevenit și dacă nu ați făcut-o până acum, nu uita să te abonezi și să apeși clopoțelul pentru a fi întotdeauna notificat când IKatheryne face live sau postează! :purple_heart:"
         await channel.send(mesajfinal)
