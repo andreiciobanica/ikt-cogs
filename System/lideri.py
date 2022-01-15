6d08e382bbf48b6b0b1f1f5468c3deb7358cf54d
Andrei Ciobănică <37911417+andreiciobanica@users.noreply.github.com>
2022-01-15 18:57:54 +0200

    Update lideri.py

--- System/lideri.py
+++ System/lideri.py
@@ -18,14 +18,176 @@ else:
 
 OVERFLOW_ERROR = "The time set is way too high, consider setting something reasonable."
 
+class lideri_grade(commands.Cog):
+    politie_grade = [
+        865215533573341184, # Lider Politie
+        865215532654788648, # Chestor Sef
+        900110938563313675, # Comisar Sef
+        865215529807904809, # Comisar
+        865215529039953923, # Sub Comisar
+        865215528472805416  # Tester
+    ]
+    roluri_politie = [
+        865215527639449620, # Agent Principal
+        865215525608357958, # Agent
+        865215524782866432  # Cadet
+    ]
+    roluri_rutiera = [
+        908792926207881276, # Lider Rutiera
+        908793125475082250  # Rutiera
+    ]
+    sias_grade = [
+        865215521094631435, # General SIAS
+        865215520445431838, # Co-General SIAS
+        865215519523209246, # Procuror SIAS
+        865215518650531870, # Sub Procuror SIAS
+        865215516507504641  # Tester
+    ]
+    roluri_sias = [
+        865215515210416179, # Coordonator SIAS
+        865215514624131072, # Agent Special SIAS
+        865215513800867850  # Agent SIAS
+    ]
+    smurd_grade = [
+        865215510265331712, # Director SMURD
+        865215509178875934, # Sef de Statie
+        885474291410407444  # Tester SMURD
+    ]
+    roluri_smurd = [
+        865215507989004328, # Medic
+        865215507183566908, # Paramedic
+        865215506239324180  # Asistent
+    ]
+    faction_restriction = [
+        865215523080241192, # Politia Romana
+        865215512098242570, # SIAS
+        865215505023238155, # Medic
+        885162367149809664  # Hitman
+    ]
+    roluri_lider = [
+            865215495623934012, # Araba
+            865215437531512842, # Triads
+            865215444150648853, # OTF
+            865215457672822815, # Siciliana
+            865215465290334278, # Ballas
+            865215472979279872, # Clanul Sportivilor
+            865215480600199188, # Groove Street
+            865215487864078357, # Bratva
+            865215503320481812, # Bloods
+            865215450883555338, # Arizona
+            885152622842101790, # Gommora
+            899760338436780042, # Racoons
+            892118179269210152, # Los Vagos
+            908442214047293450, # Sons of Anarchy
+            918482495975084092, # Tokyo Manji
+            920324660997001227, # 6rats
+            929494693585227856, # Cosa Nostra
+            929489148987981824, # Crips
+            929489154079871008, # Loz Aztecas
+            865215430662291496, # Hitman
+            865215533573341184, # Politie
+            865215521094631435, # SIAS
+            865215510265331712  # SMURD
+            ]
+    roluri_colider = [
+            865215494813777960, # Araba
+            865215436748750849, # Triads
+            865215443546537995, # OTF
+            865215457161904168, # Siciliana
+            865215464363393104, # Ballas
+            865215471640248321, # Clanul Sportivilor
+            865215479358554123, # Groove Street
+            865215486693605377, # Bratva
+            865215502014742559, # Bloods
+            865215449708625932, # Arizona
+            865856937534029825, # Gommora
+            903411093593001994, # Racoons
+            892564801279123476, # Los Vagos
+            908443445234901042, # Sons of Anarchy
+            918482448940171324, # Tokyo Manji
+            920324980758155295, # 6rats
+            929489167438708806, # Cosa Nostra
+            929489144936271893, # Crips
+            929489155854061578, # Loz Aztecas
+            865215429869961237, # Hitman
+            865215532654788648, # Politia Romana
+            865215520445431838, # SIAS
+            865215509178875934  # Sef de Statie
+            ]
+    roluri_tester = [
+            865215493908463626, # Araba
+            865215435707252737, # Triads
+            865215442721570846, # OTF
+            865215456226967574, # Siciliana
+            865215463491239936, # Ballas
+            865215470801780736, # Clanul Sportivilor
+            865215478807789578, # Groove Street
+            865215486098014208, # Bratva
+            865215501537378344, # Bloods
+            865215449243582494, # Arizona
+            885154848390127676, # Gommora
+            903411278939316235, # Racoons
+            892564804898803762, # Los Vagos
+            908443563346501652, # Sons of Anarchy
+            918482393143345192, # Tokyo Manji
+            920325133091078155, # 6rats
+            929489166494998578, # Cosa Nostra
+            929489137231343656, # Crips
+            929489156906836020, # Loz Aztecas
+            865215429039226890, # Hitman
+            865215528472805416, # Politia Romana
+            865215516507504641, # SIAS
+            885474291410407444  # SMURD
+            ]
+    roluri_membru = [
+            865215492935385118, # Araba
+            865215434935762955, # Triads
+            865215441818222592, # OTF
+            865215455261491220, # Siciliana
+            865215462232293378, # Ballas
+            865215469371654185, # Clanul Sportivilor
+            865215477432320030, # Groove Street
+            865215484986392577, # Bratva
+            865215500698648587, # Bloods
+            865215448470650900, # Arizona
+            885155574428360775, # Gommora
+            903411660042174465, # Racoons
+            892564807771897957, # Los Vagos
+            908443944818454569, # Sons of Anarchy
+            918482112280141825, # Tokyo Manji
+            920325233016205322, # 6rats
+            929489165773570068, # Cosa Nostra
+            929489128075173939, # Crips
+            929489157955391498, # Loz Aztecas
+            865215428170219560  # Hitman
+            ]
+    id_factiune = [
+            865215490782789632, # Araba
+            865215433174155344, # Triads
+            865215440182706196, # OTF
+            865215453235642410, # Siciliana
+            865215460815405056, # Ballas
+            865215467727224852, # Clanul Sportivilor
+            865215475835076678, # Groove Street
+            865215482767736833, # Bratva
+            865215498564534292, # Bloods
+            865215446848503808, # Arizona
+            885156306397315152, # Gomorra
+            903411840732790835, # Racoons
+            892564826671435847, # Los Vagos
+            908444404283490305, # Sons of Anarchy
+            918481525140488193, # Tokyo Manji
+            920325822852767744, # 6rats
+            929489164850831400, # Cosa Nostra
+            929489120433147905, # Crips
+            929489158584545372, # Loz Aztecas
+            931739070688817212, # Hitman
+            865215523586048001, # Politia Romana
+            865215512602345473, # SIAS
+            931916048817586188  # SMURD
+            ]
 
-class TempRole(commands.Cog):
-    """
-    Assign Temporary Roles
-    Give temporary roles to users, expiring after a set time.
-    """
-
-    def __init__(self, bot):
+    def __init__(self, bot, *args, **kwargs):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=14000605, force_registration=True)
         default_guild = {
@@ -38,10 +200,10 @@ class TempRole(commands.Cog):
         }
         self.config.register_guild(**default_guild)
         self.config.register_member(**default_member)
-
+        
         self.tr_handler_task = self.bot.loop.create_task(self._tr_handler())
-
-    def cog_unload(self):
+        
+        def cog_unload(self):
         self.tr_handler_task.cancel()
 
     @commands.guild_only()
@@ -300,180 +462,6 @@ class TempRole(commands.Cog):
                         member.guild,
                         f"TempRole {role.mention} for {member.mention} was unable to be removed due to a lack of permissions."
                     )
-
-class lideri_grade(commands.Cog):
-    politie_grade = [
-        865215533573341184, # Lider Politie
-        865215532654788648, # Chestor Sef
-        900110938563313675, # Comisar Sef
-        865215529807904809, # Comisar
-        865215529039953923, # Sub Comisar
-        865215528472805416  # Tester
-    ]
-    roluri_politie = [
-        865215527639449620, # Agent Principal
-        865215525608357958, # Agent
-        865215524782866432  # Cadet
-    ]
-    roluri_rutiera = [
-        908792926207881276, # Lider Rutiera
-        908793125475082250  # Rutiera
-    ]
-    sias_grade = [
-        865215521094631435, # General SIAS
-        865215520445431838, # Co-General SIAS
-        865215519523209246, # Procuror SIAS
-        865215518650531870, # Sub Procuror SIAS
-        865215516507504641  # Tester
-    ]
-    roluri_sias = [
-        865215515210416179, # Coordonator SIAS
-        865215514624131072, # Agent Special SIAS
-        865215513800867850  # Agent SIAS
-    ]
-    smurd_grade = [
-        865215510265331712, # Director SMURD
-        865215509178875934, # Sef de Statie
-        885474291410407444  # Tester SMURD
-    ]
-    roluri_smurd = [
-        865215507989004328, # Medic
-        865215507183566908, # Paramedic
-        865215506239324180  # Asistent
-    ]
-    faction_restriction = [
-        865215523080241192, # Politia Romana
-        865215512098242570, # SIAS
-        865215505023238155, # Medic
-        885162367149809664  # Hitman
-    ]
-    roluri_lider = [
-            865215495623934012, # Araba
-            865215437531512842, # Triads
-            865215444150648853, # OTF
-            865215457672822815, # Siciliana
-            865215465290334278, # Ballas
-            865215472979279872, # Clanul Sportivilor
-            865215480600199188, # Groove Street
-            865215487864078357, # Bratva
-            865215503320481812, # Bloods
-            865215450883555338, # Arizona
-            885152622842101790, # Gommora
-            899760338436780042, # Racoons
-            892118179269210152, # Los Vagos
-            908442214047293450, # Sons of Anarchy
-            918482495975084092, # Tokyo Manji
-            920324660997001227, # 6rats
-            929494693585227856, # Cosa Nostra
-            929489148987981824, # Crips
-            929489154079871008, # Loz Aztecas
-            865215430662291496, # Hitman
-            865215533573341184, # Politie
-            865215521094631435, # SIAS
-            865215510265331712  # SMURD
-            ]
-    roluri_colider = [
-            865215494813777960, # Araba
-            865215436748750849, # Triads
-            865215443546537995, # OTF
-            865215457161904168, # Siciliana
-            865215464363393104, # Ballas
-            865215471640248321, # Clanul Sportivilor
-            865215479358554123, # Groove Street
-            865215486693605377, # Bratva
-            865215502014742559, # Bloods
-            865215449708625932, # Arizona
-            865856937534029825, # Gommora
-            903411093593001994, # Racoons
-            892564801279123476, # Los Vagos
-            908443445234901042, # Sons of Anarchy
-            918482448940171324, # Tokyo Manji
-            920324980758155295, # 6rats
-            929489167438708806, # Cosa Nostra
-            929489144936271893, # Crips
-            929489155854061578, # Loz Aztecas
-            865215429869961237, # Hitman
-            865215532654788648, # Politia Romana
-            865215520445431838, # SIAS
-            865215509178875934  # Sef de Statie
-            ]
-    roluri_tester = [
-            865215493908463626, # Araba
-            865215435707252737, # Triads
-            865215442721570846, # OTF
-            865215456226967574, # Siciliana
-            865215463491239936, # Ballas
-            865215470801780736, # Clanul Sportivilor
-            865215478807789578, # Groove Street
-            865215486098014208, # Bratva
-            865215501537378344, # Bloods
-            865215449243582494, # Arizona
-            885154848390127676, # Gommora
-            903411278939316235, # Racoons
-            892564804898803762, # Los Vagos
-            908443563346501652, # Sons of Anarchy
-            918482393143345192, # Tokyo Manji
-            920325133091078155, # 6rats
-            929489166494998578, # Cosa Nostra
-            929489137231343656, # Crips
-            929489156906836020, # Loz Aztecas
-            865215429039226890, # Hitman
-            865215528472805416, # Politia Romana
-            865215516507504641, # SIAS
-            885474291410407444  # SMURD
-            ]
-    roluri_membru = [
-            865215492935385118, # Araba
-            865215434935762955, # Triads
-            865215441818222592, # OTF
-            865215455261491220, # Siciliana
-            865215462232293378, # Ballas
-            865215469371654185, # Clanul Sportivilor
-            865215477432320030, # Groove Street
-            865215484986392577, # Bratva
-            865215500698648587, # Bloods
-            865215448470650900, # Arizona
-            885155574428360775, # Gommora
-            903411660042174465, # Racoons
-            892564807771897957, # Los Vagos
-            908443944818454569, # Sons of Anarchy
-            918482112280141825, # Tokyo Manji
-            920325233016205322, # 6rats
-            929489165773570068, # Cosa Nostra
-            929489128075173939, # Crips
-            929489157955391498, # Loz Aztecas
-            865215428170219560  # Hitman
-            ]
-    id_factiune = [
-            865215490782789632, # Araba
-            865215433174155344, # Triads
-            865215440182706196, # OTF
-            865215453235642410, # Siciliana
-            865215460815405056, # Ballas
-            865215467727224852, # Clanul Sportivilor
-            865215475835076678, # Groove Street
-            865215482767736833, # Bratva
-            865215498564534292, # Bloods
-            865215446848503808, # Arizona
-            885156306397315152, # Gomorra
-            903411840732790835, # Racoons
-            892564826671435847, # Los Vagos
-            908444404283490305, # Sons of Anarchy
-            918481525140488193, # Tokyo Manji
-            920325822852767744, # 6rats
-            929489164850831400, # Cosa Nostra
-            929489120433147905, # Crips
-            929489158584545372, # Loz Aztecas
-            931739070688817212, # Hitman
-            865215523586048001, # Politia Romana
-            865215512602345473, # SIAS
-            931916048817586188  # SMURD
-            ]
-
-    def __init__(self, bot, *args, **kwargs):
-        self.bot = bot
-        global tz
-        tz = timezone("Europe/Bucharest")
 
     @commands.command(name="colider", pass_context=True)
     async def colider(self, ctx, user: discord.Member):
