1c4ac35f008a837607329465a597386c0c772196
Andrei Ciobănică <37911417+andreiciobanica@users.noreply.github.com>
2022-01-15 18:52:57 +0200

    Update __init__.py

--- System/__init__.py
+++ System/__init__.py
@@ -1,4 +1,11 @@
+import json
+from pathlib import Path
+from .lideri import TempRole
 from .lideri import lideri_grade
+
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 async def setup(bot):
     bot.add_cog(lideri_grade(bot))
+    bot.add_cog(TempRole(bot))
