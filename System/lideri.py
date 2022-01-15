190e4122850109b1c5664e8b69b0eb07b28cf883
Andrei Ciobănică <37911417+andreiciobanica@users.noreply.github.com>
2022-01-15 05:30:37 +0200

    Update lideri.py

--- System/lideri.py
+++ System/lideri.py
@@ -10,10 +10,8 @@ class lideri_grade(commands.Cog):
         self.bot = bot
         global tz
         tz = timezone("Europe/Bucharest")
-
-    @commands.command(name="colider", pass_context=True)
-    async def grade(self, ctx, user: discord.Member):
-        roluri_lider = [
+        
+                roluri_lider = [
             865215495623934012, # Araba
             865215437531512842, # Triads
             865215444150648853, # OTF
@@ -123,6 +121,9 @@ class lideri_grade(commands.Cog):
             929489158584545372, # Loz Aztecas
             931739070688817212  # Hitman
             ]
+
+    @commands.command(name="colider", pass_context=True)
+    async def grade(self, ctx, user: discord.Member):
         for x in roluri_lider:
             for y in ctx.author.roles:
                 if ctx.guild.get_role(x)==y:
