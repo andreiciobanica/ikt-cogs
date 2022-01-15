8911e62d6606845cba9a0a82261da2deff3950dc
Andrei Ciobănică <37911417+andreiciobanica@users.noreply.github.com>
2022-01-15 03:49:18 +0200

    Update lideri.py

--- System/lideri.py
+++ System/lideri.py
@@ -14,6 +14,6 @@ class lideri_grade(commands.Cog):
     @commands.command(name="grade")
     async def grade(self, ctx):
         role = ctx.guild.get_role(440955056750198795)
-        if role in ctx.author.roles:
+        if any(role in ctx.author.roles):
             await ctx.message.author.send(role)
             
