97b8677c3a8e14c83100ccd6c06228387fc505a1
Andrei Ciobănică <37911417+andreiciobanica@users.noreply.github.com>
2022-07-21 20:53:47 +0300

    Update perms.py

--- PermisiuniImperial/perms.py
+++ PermisiuniImperial/perms.py
@@ -80,19 +80,48 @@ class permisiuni(commands.Cog):
         999429537622396988
     ]
     
+    
+    politiegrade = [
+    
+    ]
+    
+    politiegrade = [
+    
+    ]
+    
+    politiegrade = [
+    
+    ]
 
     def __init__(self, bot, *args, **kwargs):
         self.bot = bot
         global tz
         tz = timezone("Europe/Bucharest")
 
-    @commands.command(name="permisiuni")
-    async def permisiuni(self, ctx):
-        embed=discord.Embed(title="Metodă de verificare", description="Pentru a putea avea acces la restul server-ului, trebuie să reacționezi utilizând emoticonul <:imperial:999590871743868940>.", color=0xfff100)
-        embed.set_author(name="Imperial România", icon_url="https://media.discordapp.net/attachments/928411916857126973/928426116253892608/novnbdalsjkvsanbpov.png")
-        embed.set_image(url="https://cdn.discordapp.com/attachments/928411916857126973/928436594988449812/standard_4.gif")
-        embed.set_footer(text="All Right Reserved ©️ Imperial România")
-        await ctx.send(embed=embed)
+    @commands.command(name="rapoartepolitie")
+    async def rapoartepd(self, ctx, idJoc, idDiscord):
+          verif = False
+            for x in permisiuni.politiegrade:
+                for y in ctx.author.roles:
+                    if ctx.guild.get_role(x)==y:
+                        name = 'rapoarte-' + idJoc
+                        await ctx.guild.create_text_channel(name, category=999720906303746168)
+                        verif = True
+                if verif == True:
+                    break
+            
+#     @commands.command(name="rapoartediicot")
+#     async def rapoartepd(self, ctx):
+#           await ctx.guild.create_text_channel('', category=999720937295446067)
+            
+#     @commands.command(name="rapoartesmurd")
+#     async def rapoartepd(self, ctx):
+#           await ctx.guild.create_text_channel('', category=999720963849605190)
+#         embed=discord.Embed(title="Metodă de verificare", description="Pentru a putea avea acces la restul server-ului, trebuie să reacționezi utilizând emoticonul <:imperial:999590871743868940>.", color=0xfff100)
+#         embed.set_author(name="Imperial România", icon_url="https://media.discordapp.net/attachments/928411916857126973/928426116253892608/novnbdalsjkvsanbpov.png")
+#         embed.set_image(url="https://cdn.discordapp.com/attachments/928411916857126973/928436594988449812/standard_4.gif")
+#         embed.set_footer(text="All Right Reserved ©️ Imperial România")
+#         await ctx.send(embed=embed)
 #         manager = 999558907800322141
 #         everyone = ctx.guild.get_role(999501365594292296) #ctx.guild.default_role
 #         for x in permisiuni.politie:
